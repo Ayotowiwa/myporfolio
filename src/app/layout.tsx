b142908506944dceb9efd0f4abd4727e4f8178fe
Nuel <christlightsied@gmail.com>
2024-01-15 09:23:28 +0000

    minor change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ const poppins = Poppins({
 
 export const metadata: Metadata = {
   title: "Tomiwa Porfolio",
-  description: "Website portfolio of Adesipe tomide",
+  description: "Website portfolio of Atanniyi Ayotomiwa",
 };
 
 export default function RootLayout({
